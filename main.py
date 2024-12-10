8f7daaaa2bd3bfffc8bc2b38c1a3e51817d499ad
Ashish Anand <111524396+aashish-aanand@users.noreply.github.com>
2024-12-10 12:50:35 +0530

    Update main.py
    Adding CNN function

--- main.py
+++ main.py
@@ -32,7 +32,32 @@ validation_loader = DataLoader(validation_dataset, batch_size=32, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 # Ashish's Part
+class CNNModel(nn.Module):
+    def _init_(self, num_classes):
+        super(CNNModel, self)._init_()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 28 * 28, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes)
+        )
 
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
 
 num_classes = len(train_dataset.classes)  
 model = CNNModel(num_classes).to(device)
