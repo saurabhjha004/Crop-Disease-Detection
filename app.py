7bc80969f98725b176f18deb054da71bcce95295
Akshuu24 <163445420+Akshuu24@users.noreply.github.com>
2024-12-10 13:32:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,32 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 
 # Anushka's Part
+class CNNModel(nn.Module):
+    def _init_(self, num_classes):
+        super(CNNModel, self)._init_()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 28 * 28, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes),
+        )
 
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
 
 # Prakhar's Part
 
